ea97307cd021fdfce4ed8372f633de2e6c57938f
Kārlis Sīmanis <93652893+kaarleenss@users.noreply.github.com>
2023-03-02 23:23:38 +0000

    finished_code

--- tree_height.py
+++ tree_height.py
@@ -27,13 +27,6 @@ def compute_height(n, parent):
     
     calc_height(sakums)
     
-        # for i in range(n):
-        #     max_height = 0
-        #     for child in np.where(parent == i)[0]:
-        #         zari = heights[child]
-        #         max_height = max(max_height, zari)
-
-        #     heights[i] = max_height + 1
 
         
     return np.max(heights)
@@ -73,7 +66,7 @@ def main():
     # In Python, the default limit on recursion depth is rather low,
     # so raise it here for this problem. Note that to take advantage
     # of bigger stack, we have to launch the computation in a new thread.
-    #sys.setrecursionlimit(10**7)  # max depth of recursion
-    #threading.stack_size(2**27)   # new thread will get stack of such size
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
 threading.Thread(target=main).start()
     # print(numpy.array([1,2,3]))
